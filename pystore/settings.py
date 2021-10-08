0f8ef42a732264469131ef7a064fb41d9da0c1ca
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2021-10-08 15:26:13 -0300

    Add: Dotenv

--- pystore/settings.py
+++ pystore/settings.py
@@ -9,11 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import environ
-
-env = environ.Env()
-
-environ.Env.read_env()
+import os
 
 from pathlib import Path
 
@@ -28,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,8 +79,12 @@ WSGI_APPLICATION = 'pystore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "postgres",
+        'USER': "postgres",
+        'PASSWORD': os.environ.get("PASSWORD_DB"),
+        'HOST': 'db',
+        "PORT": 5432
     }
 }
 
