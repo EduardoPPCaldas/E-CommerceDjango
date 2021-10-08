fe5671c8d06e14391d96333e833d70a808d5b7a2
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2021-10-08 12:56:08 -0300

    Add: env

--- pystore/settings.py
+++ pystore/settings.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env()
 
 from pathlib import Path
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1s9aiyvm2!h#)_#1$@r91pc#!f$@_2dhoaxc!3$n2xkm4s4s#_'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
