3954407b558aa296f4286f45e9dd458351bea335
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2021-10-09 00:07:52 -0300

    Add: Docker and users app

--- pystore/settings.py
+++ pystore/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import environ
+
+env = environ.Env()
+
 
 from pathlib import Path
 
@@ -19,12 +23,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': "postgres",
         'USER': "postgres",
-        'PASSWORD': os.environ.get("PASSWORD_DB"),
+        'PASSWORD': env("PASSWORD_DB"),
         'HOST': 'db',
         "PORT": 5432
     }
